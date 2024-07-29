213715ff6e9a27497519e9ccd0af83539c3bb2db
Scarletbobcat <tien.hoan314@yahoo.com>
2024-07-29 19:11:45 -0400

    Created a base calendar for scheduling with resources as employees

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,20 +1,51 @@
 import { useEffect, useState } from 'react';
 import { DayPilotCalendar, DayPilot } from '@daypilot/daypilot-lite-react';
 
+interface Employee {
+    "employeeID": number,
+    "employeeName": string,
+}
+
 function Calendar() {
-    const [employees, setEmployees] = useState([]);
+    const [employees, setEmployees] = useState<Employee[]>([]);
+    const [newEmployees, setNewEmployees] = useState<DayPilot.CalendarColumnData[]>([]);
 
     useEffect(() => {
-        fetch("http://localhost:8080/Employees").then(response => response.json()).then(data => setEmployees(data)).catch(error => console.log(error));
-    }, []) 
+        const fetchEmployees = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/Employees");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data: Employee[] = await response.json();
+                setEmployees(data);
+            } catch(error) {
+                console.log(error);
+            }
+        };
+        
+        fetchEmployees();
+        setNewEmployees(employees.map((e) => {
+            return {
+                "id": e.employeeID,
+                "name": e.employeeName,
+            }
+        }));
+        console.log(employees);
+        console.log(newEmployees);
+        
+    },[])
 
-    const [config, setConfig] = useState({
-        viewType: "Resources",
-        columns: employees,
-    })
+
+
 
     return (
         <div>
+            <DayPilotCalendar 
+                viewType="Resources"
+                columns={newEmployees}
+                startDate={DayPilot.Date.today()}
+            />
         </div>
     )
 }
